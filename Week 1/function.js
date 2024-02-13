a363d710b3f2c5ab1d103ca342b45927e3a7fff4
Vivek Gupta <91420760+vivekot@users.noreply.github.com>
2024-02-14 00:57:21 +0530

    Add files via upload

---
+++ Week 1/function.js
@@ -0,0 +1,12 @@
+function findSum(n){
+    let ans=0;
+    for(let i=1;i<n;i++){
+      ans=ans+i;
+    }
+    return ans;
+  }
+
+  let ans=findsum(100);
+console.log(ans);
+  
+  
---
+++ Week 1/readFromFile.js
@@ -0,0 +1,5 @@
+// node js library
+const fs=require(fs);
+fs.readFile(a.txt,utf-8,function(err,data){
+  console.log(data);
+})
